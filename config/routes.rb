3e42489c525af41e8cb609f743cbe0ba5753570f
Shintaro Kawafune <djdtapg@yahoo.co.jp>
2020-02-07 09:13:11 +0800

    [Layout & Function] CreateStaticPages(HomePage)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/new'
+  # Users
+  get "/signup" , to: "user#new"
+  # Login and Logout
+  get "/login", to: "sessions#new"
+
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
