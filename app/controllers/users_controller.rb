591f07c30e137f40596c6d224a18dfa54483e546
Kawafunes <55682248+Kawafunes@users.noreply.github.com>
2020-02-10 12:24:39 +0800

    Create sign-up function (#3)
    * Create sign-up function
    
    * sign-up-f

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Registerd new user"
+      redirect_to @user
+    else
+      flash.now[:danger] = 'Failed to sign-up'
+      render "new"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
