4af7d495b399a57aa0904e453e136aaa62438744
Shintaro Kawafune <djdtapg@yahoo.co.jp>
2020-02-11 09:16:13 +0800

    revise session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,4 @@
 class SessionsController < ApplicationController
   def new
   end
-
-  def create
-    user = User.find_by(email: params[:session][:email].downcase)
-
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to @user #Goes to show page
-    else
-       render "new"
-    end
-  end
-
-  # This is Sign out button
-  def destroy
-    log_out
-    redirect_to root_url
-  end
 end
-
