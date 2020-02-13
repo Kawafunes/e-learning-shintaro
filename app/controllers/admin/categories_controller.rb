2af328cf23943a3c845c590fe3cec1c5d916e1e2
Shintaro Kawafune <djdtapg@yahoo.co.jp>
2020-02-13 11:37:57 +0800

    [Function] Create Category Page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,5 +3,22 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash.now[:success] = "Add New Category"
+      redirect_to admin_categories_url
+    else
+      flash.now[:danger] = 'Failed to sign-up'
+      render "new"
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:title, :description)
   end
 end
