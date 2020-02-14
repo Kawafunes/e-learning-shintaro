4ecfe0c05ee4f4468af06c05be10acf52db0861c
Shintaro Kawafune <djdtapg@yahoo.co.jp>
2020-02-14 10:09:59 +0800

    [Layout] Index Category Page

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,3 +2,8 @@ class Category < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
 end
+
+def index
+  @title = title.all
+  @description = description.all
+end
