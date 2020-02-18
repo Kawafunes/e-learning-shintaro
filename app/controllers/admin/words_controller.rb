ceaccf52d8e7746b21a2c1469b19d18fd524c8ea
Kawafunes <55682248+Kawafunes@users.noreply.github.com>
2020-02-18 12:14:17 +0800

    [Function] Create Words Page (#14)

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,5 +1,22 @@
 class Admin::WordsController < ApplicationController
   def new
-    @word = Word.new
+    @category = Category.find(params[:category_id])
+    @word = @category.words.new
+    3.times { @word.choices.new}
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @word = @category.words.new(word_params) 
+    if @word.save
+      redirect_to admin_category_url(@category)
+    else
+      render "new"
+    end
+end
+
+private
+  def word_params
+    params.require(:word).permit(:content, choices_attributes: [:id, :content, :is_correct])
   end
 end
