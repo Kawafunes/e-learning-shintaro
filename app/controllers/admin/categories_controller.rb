9fc2fff3b5642664f1b7dadc2ba649981d06e5e6
Kawafunes <55682248+Kawafunes@users.noreply.github.com>
2020-02-20 09:20:52 +0800

    [Layout] Lesson List Page (#17)
    * [Layout] Lesson List Page
    
    * [Layout] Lesson List Page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    @categories = Category.paginate(page: params[:page], per_page: 6)
   end
 
   def new
