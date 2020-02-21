a1a514b18fb38f526dd0c682cd9282cb51463ff3
Shintaro Kawafune <djdtapg@yahoo.co.jp>
2020-02-21 10:21:42 +0800

    [Function] Lesson answering function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :lessons
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,8 @@ Rails.application.routes.draw do
       resources :words
     end
   end
+
+  resources :lessons do
+    resources :answers
+  end
 end
