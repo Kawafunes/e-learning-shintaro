a82f7746c02692c7cee08278fa4c162d86fcc5eb
Kawafunes <55682248+Kawafunes@users.noreply.github.com>
2020-02-21 10:24:16 +0800

    [Function] Lesson answering function (#19)

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,21 @@
 class AnswersController < ApplicationController
   def new
+    @answer = Answer.new
+    @lesson = Lesson.find(params[:lesson_id])
+    if @lesson.next_word.nil?
+      redirect_to lesson_url(@lesson)
+    end
   end
+
+  def create
+    @lesson = Lesson.find(params[:lesson_id])
+    @lesson.answers.create(answer_params)
+    redirect_to new_lesson_answer_url(@lesson)
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:word_id , :choice_id)
+end
+
 end
