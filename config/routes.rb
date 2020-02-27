6439d7885bd00a88b0ba427de271775b6c838125
Kawafunes <55682248+Kawafunes@users.noreply.github.com>
2020-02-27 10:37:13 +0800

    [Function] Follow Function (#22)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get "/signup" , to: "users#new"
 
+  resources :relationships
   resources :users
   resources :categories
   root 'static_pages#home'
