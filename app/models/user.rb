2cd65973b8869b94bc5e9131a6fddb1ec869e53e
Shintaro Kawafune <djdtapg@yahoo.co.jp>
2020-02-27 10:31:10 +0800

    [Function] Follow Function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :active_relationships, class_name: "Relationship",
+    foreign_key: "follower_id",
+    dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+    foreign_key: "followed_id",
+    dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   has_many :lessons
   validates :name, presence: true
@@ -14,5 +22,17 @@ class User < ApplicationRecord
 
   def taken_lesson(category)
     lessons.find_by(category: category)
+  end
+
+  def follow(user)
+    active_relationships.create(followed: user)
+  end
+
+  def unfollow(user)
+    active_relationships.find_by(followed: user).destroy
+  end
+
+  def following?(user)
+    following.include?(user)
   end
 end
