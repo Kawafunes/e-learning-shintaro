fe6cbedd795e2e390671306e0b898fb58ba07f7e
Shintaro Kawafune <djdtapg@yahoo.co.jp>
2020-02-28 12:32:29 +0800

    [Layout] Display Activities

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
+      @activities = Activity.where("user_id = ? OR user_id IN (?)", current_user.id, current_user.following.ids).order(created_at: :desc).paginate(page: params[:page], per_page: 6)
       render "users/dashboard"
     end
   end
